65e126ca0a1396e22a703051f0de6f05b5717838
falguni-prajapati <falguni.prajapati@truestaz.com>
2023-04-21 17:11:43 +0530

    Add .is-an.app.js File

---
+++ domains/falguni-prajapati.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'falguni-prajapati',
+  owner: {
+    repo: 'https://github.com/falguni-prajapati/falguni-prajapati.github.io',
+    email: 'falguni.prajapati@truestaz.com,
+  },
+  record: {
+    CNAME: 'falguni-prajapati.github.io',
+  },
+  proxy: false,
+})
